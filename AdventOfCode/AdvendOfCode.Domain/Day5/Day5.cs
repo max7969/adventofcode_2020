7c80f193e7ca8363e51d451628b7067df07cc1b1
maxime.andre <maxime.andr@gmail.com>
2020-12-05 09:21:44 +0100

    day5:Solution

---
+++ AdventOfCode/AdvendOfCode.Domain/Day5/Day5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using AdventOfCode.Domain.Utils;
+
+namespace AdventOfCode.Domain.Day5
+{
+    public class Day5
+    {
+        public int Compute(string filePath)
+        {
+            var lines = FileReader.GetFileContent(filePath).ToList();
+            return ReadBoardingPass(lines).Max();
+        }
+
+        public int Compute2(string filePath)
+        {
+            var lines = FileReader.GetFileContent(filePath).ToList();
+            var elements = ReadBoardingPass(lines);
+
+            var filtered = elements
+                .Where(x => !elements.Contains(x + 1) || !elements.Contains(x - 1))
+                .OrderBy(x => x).ToList();
+
+            return filtered[1] + 1;
+        }
+
+        private IEnumerable<int> ReadBoardingPass(IEnumerable<string> lines)
+        {
+            return lines.Select(x =>
+            {
+                var row = Convert.ToInt32(x.Substring(0, 7).Replace("B", "1").Replace("F", "0"), 2);
+                var column = Convert.ToInt32(x.Substring(7).Replace("R", "1").Replace("L", "0"), 2);
+                return new {row, column};
+            }).Select(x => x.row * 8 + x.column);
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Domain.Tests/Day5/Day5Test.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Reflection;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Domain.Day5
+{
+    public class Day5Test
+    {
+        private ITestOutputHelper _output;
+        public Day5Test(ITestOutputHelper output)
+        {
+        _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day5/Resources/test.txt";
+            Day5 day = new Day5();
+
+            // Act
+            var result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(567);
+        }
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day5/Resources/input.txt";
+            Day5 day = new Day5();
+
+            // Act
+            var result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath = $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day5/Resources/input.txt";
+            Day5 day = new Day5();
+
+            // Act
+            var result = day.Compute2(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
