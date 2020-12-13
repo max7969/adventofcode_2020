7ab257db54bbd0bb0e370694e370fec0ebbfeeb7
maxime.andre <maxime.andr@gmail.com>
2020-12-13 21:29:48 +0100

    day13:Solution => et sans utiliser des algos que je capte pas

---
+++ AdventOfCode/AdventOfCode.Domain.Tests/Day13/Day13Test.cs
@@ -0,0 +1,153 @@
+﻿using FluentAssertions;
+using System.IO;
+using System.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Domain.Day13
+{
+    public class Day13Test
+    {
+        private ITestOutputHelper _output;
+
+        public Day13Test(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Test1Part1()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/test.txt";
+            Day13 day = new Day13();
+
+            // Act
+            long result = day.Compute(filePath);
+
+            // Assert
+            result.Should().Be(295);
+        }
+
+        [Fact]
+        public void SolutionPart1()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/input.txt";
+            Day13 day = new Day13();
+
+            // Act
+            var result = day.Compute(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public void Test1Part2()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/test.txt";
+            Day13 day = new Day13();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(1068781);
+        }
+
+        [Fact]
+        public void Test2Part2()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/test2.txt";
+            Day13 day = new Day13();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(3417);
+        }
+
+        [Fact]
+        public void Test3Part2()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/test3.txt";
+            Day13 day = new Day13();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(1202161486);
+        }
+
+        [Fact]
+        public void Test4Part2()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/test4.txt";
+            Day13 day = new Day13();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(754018);
+        }
+
+        [Fact]
+        public void Test5Part2()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/test5.txt";
+            Day13 day = new Day13();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(779210);
+        }
+
+        [Fact]
+        public void Test6Part2()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/test6.txt";
+            Day13 day = new Day13();
+
+            // Act
+            long result = day.Compute2(filePath);
+
+            // Assert
+            result.Should().Be(1261476);
+        }
+
+        [Fact]
+        public void SolutionPart2()
+        {
+            // Arrange 
+            string filePath =
+                $"{new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName}/Day13/Resources/input.txt";
+            Day13 day = new Day13();
+
+            // Act
+            var result = day.Compute2(filePath);
+
+            // Result
+            _output.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode.Domain/Day13/Day13.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
+using AdventOfCode.Domain.Utils;
+
+namespace AdventOfCode.Domain.Day13
+{
+    public class Day13
+    {
+        public long Compute(string filePath)
+        {
+            var lines = FileReader.GetFileContent(filePath).ToList();
+            var timestamp = long.Parse(lines[0]);
+            var buses = lines[1].Split(",").Where(x => x != "x").Select(x => long.Parse(x));
+            var bus = buses.Select(x => new { number = x, wait = x - (timestamp % x) }).OrderBy(x => x.wait).First();
+            return bus.number * bus.wait;
+        }
+
+        public long Compute2(string filePath)
+        {
+            var lines = FileReader.GetFileContent(filePath).ToList();
+            var buses = lines[1].Split(",").Select(x => x != "x" ? long.Parse(x) : 1).ToList();
+            long step = 1;
+            long timestamp = 0;
+
+            for (int i = 1; i <= buses.Count; i++)
+            {
+                var busRange = buses.GetRange(0, i);
+                while (busRange.Where(x => x != 1).Any(x => (timestamp + busRange.IndexOf(x)) % x != 0))
+                {
+                    timestamp += step;
+                }
+
+                step = busRange.Aggregate((a, b) => a * b);
+
+                if (buses.Where(x => x != 1).All(x => x - (timestamp % x) == buses.IndexOf(x) + 1))
+                {
+                    break;
+                }
+            }
+
+            return timestamp;
+        }
+    }
+}
