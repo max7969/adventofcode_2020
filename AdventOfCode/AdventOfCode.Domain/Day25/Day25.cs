b09493ce2590b0c100c12a38fda8dd9fe65ba77c
Maxime André <maxime.andr@gmail.com>
2020-12-26 16:22:19 +0100

    day25:Solution

---
+++ AdventOfCode/AdventOfCode.Domain/Day25/Day25.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode.Domain.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Domain.Day25
+{
+    public class Day25
+
+    {
+        public long Compute(string filePath)
+        {
+            var lines = FileReader.GetFileContent(filePath).ToList();
+            var cardPublicKey = lines[0];
+            var doorPublicKey = lines[1];
+
+            long secretCardLoop = LoopUntil(cardPublicKey);
+            long secretDoorLoop = LoopUntil(doorPublicKey);
+
+            return GetPrivateKey(secretCardLoop, doorPublicKey);
+        }
+
+        public long LoopUntil(string key)
+        {
+            long target = long.Parse(key);
+            long value = 1;
+            long count = 0;
+            while (value != target)
+            {
+                value = value * 7;
+                value = value % 20201227;
+                count++;
+            }
+            return count;
+        }
+
+        public long GetPrivateKey(long loop, string key)
+        {
+            long value = 1;
+            long subjectNumber = long.Parse(key);
+            for (var i = 0; i < loop; i++)
+            {
+                value = value * subjectNumber;
+                value = value % 20201227;
+            }
+            return value;
+        }
+    }
+}
